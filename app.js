89dc9dbb4103b9408c9d7f291e003a8b1e8f094f
MarifulIslam2123 <remudhaka@gmail.com>
2023-12-22 23:53:31 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const userInput = document.querySelector("#userInput"); 
+const qrWidth = document.querySelector("#width");
+const qrHeight = document.querySelector("#height");
+const qrCreateBtn = document.querySelector("#qrCreateBtn");
+
+function getQrData(UserData,width,height){
+    fetch(`https://api.qrserver.com/v1/create-qr-code/?size=${width}x${height}&data=${UserData}`)
+    .then((data) => document.getElementById("qrImage").src = data.url);
+
+    //document.getElementById("qrImage").src = data.url;
+}
+
+
+qrCreateBtn.addEventListener("click",function(){
+    const UserInputData = userInput.value; 
+    const qrWidthData = qrWidth.value;
+    const qrHeightData = qrHeight.value;
+
+    document.querySelector("#userInput").value = "";
+    document.querySelector("#width").value = "";
+    document.querySelector("#height").value = "";
+    
+
+    getQrData(UserInputData, qrWidthData, qrHeightData);
+    
+
+    
+});
+
+
+// function generateQRCode() {
+//     // Get input values
+//     var inputText = document.getElementById('inputText').value;
+//     var width = document.getElementById('width').value;
+//     var height = document.getElementById('height').value;
+
+//     // Construct API URL
+//     var apiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=${width}x${height}&data=${inputText}`;
+
+//     // Set the source of the image to the generated QR code URL
+//     document.getElementById('qrcode').src = apiUrl;
+//   }
